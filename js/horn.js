b6fe4b5b8128c44e0e328eae41a69320279dba75
cantwt <cantwt22@gmail.com>
2013-12-27 18:46:39 -0500

    Still Trying
    AUUUUUDIOOOOOOO

--- js/horn.js
+++ js/horn.js
@@ -1,12 +1,16 @@
-var mySound = new buzz.sound( "AirHorn", {
-    formats: [ "ogg", "mp3" ]
-});
+var mySound;
 
-mySound.play()
-    .bind( "timeupdate", function() {
-        var timer = buzz.toTimer( this.getTime() );
-        document.getElementById( "timer" ).innerHTML = timer;
-    });
+$( document ).ready(function() {
+    buzz.defaults.formats = [ 'ogg', 'mp3' ];
+
+    mySound = new buzz.sound( "AirHorn" );
+    
+    mySound.play()
+        .bind( "timeupdate", function() {
+            var timer = buzz.toTimer( this.getTime() );
+            document.getElementById( "timer" ).innerHTML = timer;
+        });
+});
 
 function play() {
     mySound.stop();
