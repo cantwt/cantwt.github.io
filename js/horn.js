9ba3fb7c4e5aef715f72dc7d24ec4f7484ed67e7
cantwt <cantwt22@gmail.com>
2013-12-27 19:11:12 -0500

    Fix stuff

--- js/horn.js
+++ js/horn.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var sound;
 
 $( document ).ready( function() {
@@ -5,6 +6,22 @@ $( document ).ready( function() {
         urls: ['AirHorn.ogg', 'AirHorn.mp3']
     });
 });
+=======
+<<<<<<< HEAD
+var sound;
+
+$( document ).ready( function() {
+    sound = new Howl({
+        urls: ['AirHorn.ogg', 'AirHorn.mp3']
+    });
+});
+=======
+var mySound;
+
+$( document ).ready(function() {
+    buzz.defaults.formats = [ 'ogg', 'mp3' ];
+>>>>>>> parent of 8972b85... Audio Improving
+>>>>>>> Fix stuff
 
 document.addEventListener('keydown', function(event) {
     if(event.keyCode == 81) {
