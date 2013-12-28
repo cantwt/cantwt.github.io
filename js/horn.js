9a05e0228b1a182274ac391ad4c4f5b4ab2e80f4
cantwt <cantwt22@gmail.com>
2013-12-27 19:19:07 -0500

    Finally Fixed
    Things should be back to normal

--- js/horn.js
+++ js/horn.js
@@ -1,32 +1,13 @@
-<<<<<<< HEAD
-var sound;
-
-$( document ).ready( function() {
-    sound = new Howl({
-        urls: ['AirHorn.ogg', 'AirHorn.mp3']
-    });
-});
-=======
-<<<<<<< HEAD
-var sound;
-
-$( document ).ready( function() {
-    sound = new Howl({
-        urls: ['AirHorn.ogg', 'AirHorn.mp3']
-    });
-});
-=======
 var mySound;
 
 $( document ).ready(function() {
     buzz.defaults.formats = [ 'ogg', 'mp3' ];
->>>>>>> parent of 8972b85... Audio Improving
->>>>>>> Fix stuff
 
-document.addEventListener('keydown', function(event) {
-    if(event.keyCode == 81) {
-        sound.stop();
-    }
+    document.addEventListener('keydown', function(event) {
+        if(event.keyCode == 81) {
+            sound.stop();
+        }
+    });
 });
 
 function play () {
